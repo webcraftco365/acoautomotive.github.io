1ffe145d6ef2d28bf1d82835b51fb92353b90fb7
webcraftco365 <webcraftco365@gmail.com>
2025-10-02 10:04:02 -0700

    code

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: Button click alert
+document.querySelectorAll(".btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Thanks for choosing ProMotors! Our team will contact you soon.");
+  });
+});
+
